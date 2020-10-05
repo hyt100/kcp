7d3108bd0344ef8dc1228dcfc0910b8b2c0ecac1
hyt100 <hyt100_code@126.com>
2020-10-05 20:27:13 +0800

    Reduce unused memory

--- ikcp.c
+++ ikcp.c
@@ -254,7 +254,7 @@ ikcpcb* ikcp_create(IUINT32 conv, void *user)
 	kcp->mss = kcp->mtu - IKCP_OVERHEAD;
 	kcp->stream = 0;
 
-	kcp->buffer = (char*)ikcp_malloc((kcp->mtu + IKCP_OVERHEAD) * 3);
+	kcp->buffer = (char*)ikcp_malloc(kcp->mtu);
 	if (kcp->buffer == NULL) {
 		ikcp_free(kcp);
 		return NULL;
@@ -1254,7 +1254,7 @@ int ikcp_setmtu(ikcpcb *kcp, int mtu)
 	char *buffer;
 	if (mtu < 50 || mtu < (int)IKCP_OVERHEAD) 
 		return -1;
-	buffer = (char*)ikcp_malloc((mtu + IKCP_OVERHEAD) * 3);
+	buffer = (char*)ikcp_malloc(mtu);
 	if (buffer == NULL) 
 		return -2;
 	kcp->mtu = mtu;
